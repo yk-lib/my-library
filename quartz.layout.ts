bdc4c7ee3e4ffab7eb70fc8aaba1c766064cb237
yk-lib <yokomura726@gmail.com>
2026-03-25 19:32:26 +0900

    update: 最新記事一覧を追加

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,14 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    // ここに追加
+    Component.RecentNotes({
+      title: "最近の更新",
+      limit: 5, // 表示する記事数
+      filter: (f) => f.slug! !== "index", // ホームページ自身を除外
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
@@ -24,12 +31,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
-    // ここに追加
-    Component.RecentNotes({
-      title: "最近の更新",
-      limit: 5, // 表示する記事数
-      filter: (f) => f.slug! !== "index", // ホームページ自身を除外
-    }),
   ],
   left: [
     Component.PageTitle(),
